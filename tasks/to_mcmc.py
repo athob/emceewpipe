46d5a6903e348293a1aa08e358e3efc4bec9b76d
Adrien Thob <adrien.thob@gmail.com>
2022-07-29 23:41:55 -0400

    Fixing caching performance leak issue, part 7

--- tasks/to_mcmc.py
+++ tasks/to_mcmc.py
@@ -36,27 +36,29 @@ def make_new_model(*args):
     wp.ThisJob.logprint(' '.join([f"{n}x{t[:5]}" for t,n in zip(*np.unique(list(map(lambda I: type(I).__name__,wp.si.INSTANCES)), return_counts=True))]))
     wp.ThisJob.logprint(f"DataProduct.__cache__.shape = {wp.DataProduct.__cache__.shape}")
     models = update_models()
-    # CLEAR CACHE, bit of a clumsy fix there...
+    # CLEAR CACHE, bit of a clumsy fix there...-----------------------------------------
     wp.ThisJob.logprint(f"Before clear len(INSTANCES) = {len(wp.si.INSTANCES)}")
     wp.ThisJob.logprint(' '.join(["Before clear"]+[f"{n}x{t[:5]}" for t,n in zip(*np.unique(list(map(lambda I: type(I).__name__,wp.si.INSTANCES)), return_counts=True))]))
     wp.ThisJob.logprint(f"Before clear DataProduct.__cache__.shape = {wp.DataProduct.__cache__.shape}")
     _temp = wp.DataProduct.__cache__.groupby('group').get_group('proc').query("filename != 'Cache.h5'")
     # Clear options
+    _tmp_opt = wp.Option.__cache__.query("optowner_id in @_temp.dp_id")
     _options = pd.DataFrame(
         [tuple(opt._sa_instance_state.dict[k] for k in ['optowner_id', 'id'])+(opt,)
          for opt in wp.si.INSTANCES if 'id' in opt._sa_instance_state.dict.keys() and isinstance(opt, wp.si.Option)],
          columns=['own_id', 'id', 'opt']).query("own_id in @_temp.dp_id").drop(columns='own_id')
-    wp.si.INSTANCES = set(wp.si.INSTANCES)-set(_options.opt)
+    wp.si.INSTANCES = list(set(wp.si.INSTANCES)-set(_options.opt)-{opt._option for opt in _tmp_opt.option})
+    wp.Option.__cache__.drop(_tmp_opt.index, inplace=True)
     wp.Option.__cache__.drop(wp.Option.__cache__.query("option_id in @_options.id").index, inplace=True)
     # Clear dataproducts
     wp.si.INSTANCES = list(set(wp.si.INSTANCES)-{dp._dataproduct for dp in _temp.dataproduct})
     wp.DataProduct.__cache__.drop(_temp.index, inplace=True)
-    del _options, _temp
+    del _tmp_opt, _options, _temp
     gc.collect()
     wp.ThisJob.logprint(f"After clear len(INSTANCES) = {len(wp.si.INSTANCES)}")
     wp.ThisJob.logprint(' '.join(["After clear"]+[f"{n}x{t[:5]}" for t,n in zip(*np.unique(list(map(lambda I: type(I).__name__,wp.si.INSTANCES)), return_counts=True))]))
     wp.ThisJob.logprint(f"After clear DataProduct.__cache__.shape = {wp.DataProduct.__cache__.shape}")
-    # -----------------------------------------
+    # ----------------------------------------------------------------------------------
     if len(models):
         deviations = (np.array(models.index.to_list()) - args) / dmu.CHARA_LENGTHS
         distances = linalg.norm(deviations, axis=1)
